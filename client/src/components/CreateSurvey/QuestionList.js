d4ed51041d3ef15698858d5d0f6fc2c494f422fc
mfowler38 <fowler.megan@hotmail.com>
2020-04-05 00:24:59 -0400

    update questionlist

--- client/src/components/CreateSurvey/QuestionList.js
+++ client/src/components/CreateSurvey/QuestionList.js
@@ -7,6 +7,7 @@ export default ({data}) => {
             return (
                 <li 
                     key={q.id}
+                    className = ''
                 >
                     {q.text}
                 </li>
